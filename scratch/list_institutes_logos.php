13947c91670a68c7984f25c2cd3427cdc860f2ce
tntinnovations2025 <tntinnovations2025@gmail.com>
2026-09-12 16:57:45 +0500

    feat: implement multi-portal architecture, academic modules, and permission middleware

--- scratch/list_institutes_logos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'd:/UPLYFT/uplifyt/vendor/autoload.php';
-$app = require_once 'd:/UPLYFT/uplifyt/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Http\Kernel::class)->bootstrap();
-
-foreach (\App\Models\Institute::withoutGlobalScopes()->get() as $i) {
-    echo "ID: {$i->id} | Name: {$i->name} | Logo: " . ($i->logo_path ?? 'NULL') . " | Icon: " . ($i->icon_path ?? 'NULL') . "\n";
-}
